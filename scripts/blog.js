d1d06fc23bda5eee360951b9502ef8de48ba2476
Bertrand Delacretaz <bdelacretaz@apache.org>
2024-05-16 11:14:13 +0200

    author-card

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,7 +1,24 @@
 // Blog-specific scripts
 import {
   buildBlock,
+  getMetadata
 } from './aem.js';
+
+function loadWebComponents() {
+  // List all components here that are found
+  // in our web-components folder. This function
+  // loads those which are found in the document.
+  [
+    'author-card'
+  ].forEach(name => {
+    if(document.querySelector(name)) {
+      const script = document.createElement('script');
+      script.setAttribute('src', `/web-components/${name}.js`);
+      script.setAttribute('type', 'module');
+      document.head.append(script);
+    }
+  })
+}
 
 function buildTagsBlock(main) {
   const tagsArray = [...document.head.querySelectorAll('meta[property="article:tag"]')].map((el) => el.content) || [];
@@ -9,6 +26,20 @@ function buildTagsBlock(main) {
   main.lastElementChild.append(tagsBlock);
 }
 
+function buildAuthorCardBlock(main) {
+  const title = main.querySelector('h1');
+  const author = getMetadata('author');
+  if(title && author) {
+    const c = document.createElement('author-card');
+    c.setAttribute('name', author);
+    c.setAttribute('id', getMetadata('author_id'));
+    c.setAttribute('date', getMetadata('m_date'));
+    title.parentNode.insertBefore(c, title.nextSibling);
+}
+}
+
 export async function buildBlogBlocks(main) {
   buildTagsBlock(main);
+  buildAuthorCardBlock(main);
+  loadWebComponents();
 }
---
+++ web-components/author-card.js
@@ -0,0 +1,47 @@
+
+class AuthorCard extends HTMLElement {
+  static template = document.createElement('template');
+  static style = `
+    author-card img { 
+      width:50px;
+    }
+    author-card { 
+      text-align: right;
+      font-style: italic;
+      font-size: 80%;
+    }
+  `;
+  static {
+    AuthorCard.template.innerHTML = `
+    <div>
+      By <a></a>
+      on <span></span>
+      <img>
+    </div>`;
+  }
+  static {
+    if(document.adoptedStyleSheets) {
+      const css = new CSSStyleSheet();
+      css.replaceSync(AuthorCard.style);
+      document.adoptedStyleSheets.push(css);
+    }
+  }
+  connectedCallback() {
+    const id = this.getAttribute('id');
+    const name = this.getAttribute('name');
+    const n = AuthorCard.template.content.cloneNode(true);
+
+    const a = n.querySelector('a');
+    a.textContent = name;
+    a.href = `/authors/${id}`;
+
+    n.querySelector('span').textContent = this.getAttribute('date');
+
+    const img = n.querySelector('img')
+    img.src = `/images/authors/${id}.png`;
+    img.alt = `Image of ${name}`;
+    this.append(n);
+  }
+}
+
+customElements.define('author-card', AuthorCard);
