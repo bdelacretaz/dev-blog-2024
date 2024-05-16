c015a68bbbc5faa1f63c874cf9c09535eac08cce
Bertrand Delacretaz <bdelacretaz@apache.org>
2024-05-16 10:25:59 +0200

    Fix tags

--- blocks/tags/tags.js
+++ blocks/tags/tags.js
@@ -1,13 +1,15 @@
 export default async function decorate(block) {
   const ul = document.createElement('ul');
   const tags = block.textContent.trim();
-  tags.split(',').forEach(tag => {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    a.setAttribute('href', `/tags/${tag}`);
-    a.textContent = tag;
-    li.append(a);
-    ul.append(li);
-  });
-  block.replaceChildren(ul);
+  if(tags.length > 0) {
+    tags.split(',').map(tag => tag.trim()).forEach(tag => {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.setAttribute('href', `/tags/${tag}`);
+      a.textContent = tag;
+      li.append(a);
+      ul.append(li);
+    });
+    block.replaceChildren(ul);
+  }
 }
