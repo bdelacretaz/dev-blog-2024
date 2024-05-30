2ac107ddc670f816156538cd62663963053e9300
Bertrand Delacretaz <bdelacretaz@apache.org>
2024-05-30 14:17:46 +0200

    Get all the index

--- scripts/query-index.js
+++ scripts/query-index.js
@@ -1,10 +1,11 @@
 // Access the query-index data
-// TODO cache the index, or store in local storage ??
+// TODO do we need to cache the index in a different way? Local storage ?
 let index;
 
 export async function getQueryIndex() {
   if(!index) {
-    const resp = await fetch('/query-index.json');
+    // TODO multiple requests with lower limit?
+    const resp = await fetch('/query-index.json?limit=9999');
     if(resp.ok) {
       index = JSON.parse(await resp.text());
     }
