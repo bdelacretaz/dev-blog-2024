ac8f34b2ce44ad5899763527206ba36c9873001a
Bertrand Delacretaz <bdelacretaz@apache.org>
2024-07-01 14:46:40 +0200

    Use bacom code

--- blocks/posts-textonly/posts-textonly.js
+++
@@ -1,7 +0,0 @@
-export default async function decorate(block) {
-  const pl = document.createElement('posts-list');
-  pl.setAttribute('text-only','true');
-  pl.setAttribute('select-links','true');
-  block.querySelectorAll('a').forEach(a => pl.append(a));
-  block.replaceChildren(pl);
-}
